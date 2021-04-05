79691bbf8ea67292b822bf76fc19b68e38bcaed1
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-04-05 17:51:06 -0300

    It creates the ActorServices class and implements the interface

---
+++ Actors/ActorService.cs
@@ -0,0 +1,27 @@
+public class ActorServices : IInsert, IDelete, ISelect, IUpdate
+{
+    public void Delete()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Insert()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Select()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SelectById()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+}
