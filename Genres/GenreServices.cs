04abbf3b92a3d20fe4b4e326a8919924ded6bf64
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-04-08 11:17:43 -0300

    It creates the Genres classes

---
+++ Genres/GenreServices.cs
@@ -0,0 +1,30 @@
+namespace MyMoviesDatabase.Genres
+{
+    public class GenreServices : IInsert, IUpdate, IDelete, ISelect
+    {
+        public void Delete()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Select()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SelectById()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
