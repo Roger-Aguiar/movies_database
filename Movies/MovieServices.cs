c8b174acf1f17bd5e32e99efe522ae22572e101c
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-04-08 09:33:36 -0300

    It creates the MovieService as a CRUD

---
+++ Movies/MovieServices.cs
@@ -0,0 +1,30 @@
+namespace MyMoviesDatabase.Movies
+{
+    public class MovieServices : IInsert, IUpdate, IDelete, ISelect
+    {
+        public void Delete()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Select()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SelectById()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
